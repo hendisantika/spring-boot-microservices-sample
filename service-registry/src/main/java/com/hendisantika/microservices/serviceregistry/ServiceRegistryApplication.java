4a2f018c6d657418a0079923636f8e917ee831f7
Hendi Santika <hendisantika@gmail.com>
2018-12-12 07:06:50 +0700

    #3 - Add required libs - service registry module

--- service-registry/src/main/java/com/hendisantika/microservices/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/hendisantika/microservices/serviceregistry/ServiceRegistryApplication.java
@@ -2,7 +2,23 @@ package com.hendisantika.microservices.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * The Main Spring Boot Application class that starts the Eureka discovery
+ * server since the application is annotated with {@link EnableEurekaServer}.
+ * <br>
+ * <br>
+ * <p>
+ * Note that all these annotations work in conjunction with properties defined
+ * in the external configuration files specified by the config server.
+ *
+ * @author hendisantika
+ */
+
+
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
