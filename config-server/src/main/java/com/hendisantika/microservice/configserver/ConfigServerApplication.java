c53ac54b1ac0aab6e532f7a33d6dce556b6e74d7
Hendi Santika <hendisantika@gmail.com>
2018-12-14 06:35:39 +0700

    #9 - Modify Config Server - module config server

--- config-server/src/main/java/com/hendisantika/microservice/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/hendisantika/microservice/configserver/ConfigServerApplication.java
@@ -2,7 +2,23 @@ package com.hendisantika.microservice.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * The Main Spring Boot Application class.<br>
+ * <br>
+ * <p>
+ * The {@link EnableConfigServer} annotation defines that this application will
+ * serve as the REST based API for providing external configuration. <br>
+ * <br>
+ * <p>
+ * The external repository from where the configuration will be picked up is
+ * defined in the {@linkplain application.yml} file.
+ *
+ * @author hendisantika
+ */
+
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
