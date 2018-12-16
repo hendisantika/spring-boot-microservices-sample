ecc5665b6d91a602d778f87e7e57bae88e7e2f08
Hendi Santika <hendisantika@gmail.com>
2018-12-17 05:57:29 +0700

    #21 - Modify Main App

--- auth-server/src/main/java/com/hendisantika/microservices/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/hendisantika/microservices/authserver/AuthServerApplication.java
@@ -2,8 +2,12 @@ package com.hendisantika.microservices.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableResourceServer
+@SessionAttributes("authorizationRequest")
 public class AuthServerApplication {
 
     public static void main(String[] args) {
