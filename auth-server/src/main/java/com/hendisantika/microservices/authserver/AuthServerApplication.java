3402c76b2b38989d870cc094860ca1514197601a
Hendi Santika <hendisantika@gmail.com>
2018-12-17 05:48:58 +0700

    #18 - Add auth-server module

---
+++ auth-server/src/main/java/com/hendisantika/microservices/authserver/AuthServerApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.microservices.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+}
+
