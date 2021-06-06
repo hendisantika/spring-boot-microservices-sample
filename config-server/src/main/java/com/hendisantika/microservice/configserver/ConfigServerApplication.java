b4db42188e193ab080c1320ceb79c85246ad6387
Hendi Santika <hendisantika@gmail.com>
2021-06-06 09:53:47 +0700

    #59 - Bug Fix in Config Server after Upgrading into Spring Boot 2.5.0 version

--- config-server/src/main/java/com/hendisantika/microservice/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/hendisantika/microservice/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.hendisantika.microservice.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -19,6 +20,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
